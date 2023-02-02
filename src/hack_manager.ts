5baab3eb70a2d216786ccacb83b55f29bbf394c4
Moritz R <c0derMo@users.noreply.github.com>
2023-02-03 00:42:01 +0100

    More work on the hack manager

--- src/hack_manager.ts
+++ src/hack_manager.ts
@@ -1,11 +1,100 @@
 import { NS } from "@ns";
 
+// Attack scripts
+const weakenScript = "/dist/weaken.js";
+const growScript = "/dist/grow.js";
+const hackScript = "/dist/hack.js";
+const attackScripts = [weakenScript, growScript, hackScript];
+
+// Ram reqirement for slave scripts (hack, grow, weaken)
+const slaveScriptRam = 1.75;
+
+// Delay each loop
+const loopDelay = 5000;
+
 export async function main(ns: NS): Promise<void> {
     ns.disableLog("ALL");
     const servers = new Set([]);
     await recursiveScan(ns, "home", servers);
-    ns.tprint([...servers]);
-    await ns.sleep(5000);
+    
+    while (true) {
+        // We delay at the start, so we can just continue at any point in the loop
+        await ns.sleep(loopDelay);
+
+        await nukeAll(ns, servers);
+        const mappedServers = sortServers(ns, servers);
+
+        if (mappedServers.totalFreeRam < slaveScriptRam) {
+            continue;
+        }
+        for (const target of mappedServers.hackableServers) {
+            if (isAttackOngoing(target)) {
+                continue;
+            }
+
+            // TODO: Figure out threads, launch attack
+        }
+    }
+}
+
+/**
+ * Checks whether a target server is currently under attack by our servers.
+ * @param ns NetScript functions
+ * @param targetServer Target server
+ * @param hackingServers Servers launching the attack
+ * @returns true, if the target server is currently under attack, false otherwise
+ */
+function isAttackOngoing(ns: NS, targetServer: string, hackingServers: string[]) {
+    for (const server of hackingServers) {
+        const processes = ns.ps(server);
+        for (const process of processes) {
+            if (attackScripts.includes(process.filename) && process.args.includes(targetServer)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+interface ServersMap {
+    hackableServers: string[];
+    usableServers: string[];
+    totalFreeRam: number;
+}
+
+/**
+ * Sorts and filters servers, to figure out which we can use, which we can attack
+ * @param ns NetScript functions
+ * @param servers Servers to sort
+ * @returns Sorted & filtered servers
+ */
+function sortServers(ns: NS, servers: Set<string>): ServersMap {
+    const result = { hackableServers: [], usableServers: [], totalFreeRam: 0 } as ServersMap;
+    for (const server of servers) {
+        if (ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel()
+            && ns.getServerMaxMoney(server) > 0) {
+            result.hackableServers.push(server);
+        }
+
+        if (ns.hasRootAccess(server) && server !== "home") {
+            result.usableServers.push(server);
+            const totalRam = ns.getServerMaxRam(server);
+            const usedRam = ns.getServerUsedRam(server);
+            const freeRam = totalRam - usedRam;
+            result.totalFreeRam += freeRam;
+        }
+    }
+
+    // Stolen weight from xtree
+    result.hackableServers.sort((a: string, b: string) => {
+        const serverA = ns.getServer(a);
+        const serverB = ns.getServer(b);
+        const weightA = serverA.moneyMax / serverA.minDifficulty;
+        const weightB = serverB.moneyMax / serverB.minDifficulty;
+        return weightB - weightA;
+    });
+
+    return result;
 }
 
 /**
@@ -13,7 +102,7 @@ export async function main(ns: NS): Promise<void> {
  * @param ns NetScript functions
  * @param servers Servers to attempt to hack
  */
-async function nukeAll(ns: NS, servers: string[]): Promise<void> {
+async function nukeAll(ns: NS, servers: Set<string>): Promise<void> {
     for (const server of servers) {
         if (ns.hasRootAccess(server)) continue;
         if (await ns.fileExists("BruteSSH.exe")) {
@@ -31,7 +120,10 @@ async function nukeAll(ns: NS, servers: string[]): Promise<void> {
         if (await ns.fileExists("SQLInject.exe")) {
             await ns.sqlinject(server);
         }
-        await ns.nuke(server);
+        const serverInfo = ns.getServer(server);
+        if (serverInfo.openPortCount >= serverInfo.numOpenPortsRequired) {
+            await ns.nuke(server);
+        }
     }
 }
 
